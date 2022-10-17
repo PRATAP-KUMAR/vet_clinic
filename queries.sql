7a146e9ef7233854c2f8cbbaf344c7016d0cf9ef
PRATAP-KUMAR <pratap@fastmail.fm>
2022-10-17 21:12:02 +0530

    ALL TEMPLATE FILES UPDATED

--- queries.sql
+++ queries.sql
@@ -1,3 +1,12 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name = 'Luna';
+
+SELECT NAME from animals WHERE name LIKE '%mon%';
+SELECT NAME from animals WHERE DATE_OF_BIRTH BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT NAME from animals WHERE NEUTERED = true AND ESCAPE_ATTEMPTS < 3;
+SELECT DATE_OF_BIRTH from animals WHERE NAME IN ('Agumon', 'Pikachu');
+SELECT (NAME, ESCAPE_ATTEMPTS) from animals WHERE WEIGHT_KG > 10.50;
+SELECT NAME from animals WHERE NEUTERED = true;
+SELECT NAME from animals WHERE NAME != 'Gabumon';
+SELECT NAME from animals WHERE WEIGHT_KG >=10.4 AND WEIGHT_KG <= 17.3;
--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
-
 CREATE TABLE animals (
-    name varchar(100)
+    ID INT PRIMARY KEY NOT NULL,
+    NAME TEXT NOT NULL,
+    DATE_OF_BIRTH DATE NOT NULL,
+    ESCAPE_ATTEMPTS INT NOT NULL,
+    NEUTERED BOOLEAN,
+    WEIGHT_KG DECIMAL NOT NULL
 );
